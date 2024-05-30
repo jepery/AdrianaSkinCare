72d24ec1dd8e3b75e570529b919d8c5dd40261ad
Jeffry Dela Cruz <jeperydcruz@gmail.com>
2024-05-31 01:47:02 +0800

    edit sample

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function scrollToSection(sectionId) {
     document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
 }
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.getElementById('toggle').checked = false;
+    });
+});
